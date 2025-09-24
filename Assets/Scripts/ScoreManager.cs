f42d561cbd6b70402200ca12d7b7dce9e053662e
tastyphil <kurt.abad.ywg@gmail.com>
2025-09-24 01:06:15 -0500

    refactor: added comments to Score Manager

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,14 +1,19 @@
 using UnityEngine;
+// Dont forget to import this to access UI API!
 using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
     private int score = 0;
+
+    // Component that is responsible for our Score Text
     public Text scoreText;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // Accessing the Text Component we made awhile ago, and initializing it here
+        scoreText = GetComponent<Text>();
     }
 
     // Update is called once per frame
@@ -17,6 +22,7 @@ public class ScoreManager : MonoBehaviour
         
     }
 
+    // Context Menu allows us to call this function in Unity!
     [ContextMenu("Increase Score")]
     public void IncreaseScore()
     {
