60d730ce38df64f374a1999d814a63010ddf42cc
tastyphil <kurt.abad.ywg@gmail.com>
2025-09-24 01:44:14 -0500

    refactor: added comments to Sound and AudioManager

--- Assets/Scripts/Sound.cs
+++ Assets/Scripts/Sound.cs
@@ -1,5 +1,7 @@
+// We are still using Unity API, don't forget to import this too!
 using UnityEngine;
 
+// Allows us to initialize these fields in Unity Inspector
 [System.Serializable]
 public class Sound
 {
